5dec55c4bb82f49d284e077b29f94b2b882da3b4
shahfaisal12 <faisalmkd02@gmail.com>
2023-02-26 17:08:54 +0500

    first commit

---
+++ pages/404.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import React from "react";
+
+
+const PageNotFound = () => {
+  return (
+    <div className="d-flex align-items-center" style={{ height: "80vh" }}>
+      <div className="w-75 m-auto">
+        <p className="h1 fw-bold mb-3" align="center">404</p>
+        <p align="center" className="h5 fw-bold mb-3">
+          Sorry! Page Not Found
+        </p>
+        <p align="center" className="fst-italic mb-3">
+          Sorry this content has been moved Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Earum, amet perferendis, nemo facere
+          excepturi quis.
+        </p>
+        <div align="center">
+          <Link href="/" className="text-decoration-none text-black">
+            <button type="button" class="btn btn-outline-primary btn-search">
+              GO TO HOME PAGE
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import '../styles/globals.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/js/bootstrap.bundle'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
